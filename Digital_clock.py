247f36ee1367b4750feaffaa3e22f828fc7c5551
sankalp18codder <sankalpmishra8d31@gmail.com>
2026-03-27 14:33:02 +0530

    Add digital clock implementation using Tkinter

---
+++ Digital_clock.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+from time import strftime
+
+root = tk.Tk()
+root.title("digital clock")
+
+def time():
+    string = strftime("%H:%M:%S %p \n %D")
+    label.config(text=string)
+    label.after(1000,time)
+
+label = tk.Label(root,font=("calibri", 50 , "bold"),background="black",foreground="green")
+label.pack(anchor="center")
+time()
+
+root.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
